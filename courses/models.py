f097f6279c9fa960265ae50a91161db7bcfd0d45
lebachkhoa <lebachkhoa@gmail.com>
2024-04-18 19:45:21 +0900

    add Category model

--- courses/models.py
+++ courses/models.py
@@ -4,3 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to="upload/%Y/%M")
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+
