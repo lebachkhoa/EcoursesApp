d7b1ff5457397ffd9409169811bc82831c7bad18
lebachkhoa <lebachkhoa@gmail.com>
2024-04-18 21:56:28 +0900

    ad model Category, Course, Lesson, Tag

--- courses/models.py
+++ courses/models.py
@@ -3,11 +3,49 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to="upload/%Y/%M")
+    avatar = models.ImageField(upload_to="upload/avatar/%Y/%M")
 
+
+class Base(models.Model):
+    class Meta:
+        abstract = True
+
+    subject = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    actived = models.BooleanField(default=True)
+    image = models.ImageField(upload_to="upload/courses/%Y/%M")
+
+    def __str__(self):
+        return self.subject
+    
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False)
+    category = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.category
 
 
+class Course(Base):
+    class Meta:
+        unique_together = ("subject", "category")
+    
+    description = models.CharField(max_length=1000, null=True, blank= True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+
+class Lesson(Base):
+    content = models.TextField()
+    course = models.ForeignKey(Course, models.SET_NULL, null=True)
+    tag = models.ManyToManyField("Tag")
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.tag
+    
+
+    
