9d3eef98e606c7346917b1da953d2265a9bc6ac8
dangnuduc <dangnuduc@gmail.com>
2024-04-19 21:50:49 +0900

    add updated field for Base class

--- courses/models.py
+++ courses/models.py
@@ -12,6 +12,7 @@ class Base(models.Model):
 
     subject = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     actived = models.BooleanField(default=True)
     image = models.ImageField(upload_to="upload/courses/%Y/%M")
 
